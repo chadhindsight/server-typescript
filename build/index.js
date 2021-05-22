f24ec7070540598d41e40c38480b79b636c13aef
Chad Hinds <chadrickj8@gmail.com>
2021-05-22 09:57:51 -0400

    Setup for initial push

---
+++ build/index.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+// Route handlers
+app.get('/', function (req, res) {
+    res.send("\n        <div> \n            <h1>Hello!</h1>\n        </div>\n    ");
+});
+app.listen(3000, function () {
+    console.log('Listening on Port 3000!');
+});
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+// Route handlers
+app.get('/', (req: Request, res: Response) => {
+    res.send(`
+        <div> 
+            <h1>Hello!</h1>
+        </div>
+    `)
+})
+
+app.listen(3000, () => {
+    console.log('Listening on Port 3000!')
+})
