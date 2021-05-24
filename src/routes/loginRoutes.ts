eca9726b818db4ac6713b62c09d42bb3d9c030b1
Chad Hinds <chadrickj8@gmail.com>
2021-05-24 10:31:44 -0400

    Explain what are being used via comments

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,9 +1,32 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-// Route handlers
-router.get('/', (req, res) => {
+// Route handlers.
+router.get('/', (req: Request, res: Response) => {
     res.send('Wow ')
 })
+
+router.get('/login', (req, res) => {
+    res.send(`
+         <form method="POST">
+            <div>
+                <label>Email </label>
+                <input name="email" />
+            </div>
+            <div>
+                <label>Password</label>
+                <input naem="password" type="password"/>
+            </div>
+            <button>Submit</button>
+         </form>
+    `)
+})
+
+router.post('/login', (req: Request, res: Response) => {
+    const { email, password } = req.body
+
+    res.send(email + password)
+})
+
 export { router };
