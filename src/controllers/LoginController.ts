b8e62ccbc943bf95f9302a576015c87a5d91052c
Chad Hinds <chadrickj8@gmail.com>
2021-06-09 14:06:31 -0400

    Add comment explaining use of getLogout function

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,22 +1,42 @@
-import { Router, Request, Response, NextFunction } from 'express';
-import { get } from './decorators/routes'
+import { Request, Response, NextFunction } from 'express';
+import { get, controller, bodyValidator, post } from './decorators';
 
-// @controller('/')
+@controller('/auth')
 class LoginController {
   @get('/login')
   getLogin(req: Request, res: Response): void {
     res.send(`
-        <form method="POST">
-      <div>
-      <label>Email</label>
-      <input name="email" />
-      </div>
-      <div>
-      <label>Password</label>
-      <input name="password" type="password" />
-      </div>
-      <button>submit</submit>
-        </form>
-        `);
+      <form method="POST">
+        <div>
+          <label>Email</label>
+          <input name="email" />
+        </div>
+        <div>
+          <label>Password</label>
+          <input name="password" type="password" />
+        </div>
+        <button>Submit</button>
+      </form>
+    `);
+  }
+
+  @post('/login')
+  @bodyValidator('email', 'password')
+  postLogin(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    if (email === 'hi@hi.com' && password === 'password') {
+      req.session = { loggedIn: true };
+      res.redirect('/');
+    } else {
+      res.send('Invalid email or password');
+    }
+  }
+  // Log a  user out
+  @get('/logout')
+  getLogout(req: Request, res: Response) {
+    req.session = undefined;
+    res.redirect('/');
   }
 }
+// Your boy is lost ngl
